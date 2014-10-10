b04c292ed1e25755bf746fd639f23d00e4c8d8a9
brunoslr <brunosamuellr@gmail.com>
2014-10-10 18:56:56 -0400

    Teste2

--- AStar/AStar.cpp
+++ AStar/AStar.cpp
@@ -1,5 +1,114 @@
 #include "AStar.h"
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// STL A* Search implementation
+// (C)2001 Justin Heyes-Jones
+//
+// Finding a path on a simple grid maze
+// This shows how to do shortest path finding using A*
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <stdio.h>
+
+#define DEBUG_LISTS 0
+#define DEBUG_LIST_LENGTHS_ONLY 0
+
+using namespace std;
+
+// Global data
+
+// The world map
+
+const int MAP_WIDTH = 20;
+const int MAP_HEIGHT = 20;
+
+int world_map[MAP_WIDTH * MAP_HEIGHT] =
+{
+
+	// 0001020304050607080910111213141516171819
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 00
+	1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1,   // 01
+	1, 9, 9, 1, 1, 9, 9, 9, 1, 9, 1, 9, 1, 9, 1, 9, 9, 9, 1, 1,   // 02
+	1, 9, 9, 1, 1, 9, 9, 9, 1, 9, 1, 9, 1, 9, 1, 9, 9, 9, 1, 1,   // 03
+	1, 9, 1, 1, 1, 1, 9, 9, 1, 9, 1, 9, 1, 1, 1, 1, 9, 9, 1, 1,   // 04
+	1, 9, 1, 1, 9, 1, 1, 1, 1, 9, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1,   // 05
+	1, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 1, 1, 1, 1, 1,   // 06
+	1, 9, 9, 9, 9, 9, 9, 9, 9, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 1,   // 07
+	1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1,   // 08
+	1, 9, 1, 9, 9, 9, 9, 9, 9, 9, 1, 1, 9, 9, 9, 9, 9, 9, 9, 1,   // 09
+	1, 9, 1, 1, 1, 1, 9, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 10
+	1, 9, 9, 9, 9, 9, 1, 9, 1, 9, 1, 9, 9, 9, 9, 9, 1, 1, 1, 1,   // 11
+	1, 9, 1, 9, 1, 9, 9, 9, 1, 9, 1, 9, 1, 9, 1, 9, 9, 9, 1, 1,   // 12
+	1, 9, 1, 9, 1, 9, 9, 9, 1, 9, 1, 9, 1, 9, 1, 9, 9, 9, 1, 1,   // 13
+	1, 9, 1, 1, 1, 1, 9, 9, 1, 9, 1, 9, 1, 1, 1, 1, 9, 9, 1, 1,   // 14
+	1, 9, 1, 1, 9, 1, 1, 1, 1, 9, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1,   // 15
+	1, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 1, 1, 1, 1, 1,   // 16
+	1, 1, 9, 9, 9, 9, 9, 9, 9, 1, 1, 1, 9, 9, 9, 1, 9, 9, 9, 9,   // 17
+	1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1,   // 18
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 19
+
+};
+
+// map helper functions
+
+int GetMap(int x, int y)
+{
+	if (x < 0 ||
+		x >= MAP_WIDTH ||
+		y < 0 ||
+		y >= MAP_HEIGHT
+		)
+	{
+		return 9;
+	}
+
+	return world_map[(y*MAP_WIDTH) + x];
+}
+
+
+
+// Definitions
+
+class MapSearchNode
+{
+public:
+	int x;	 // the (x,y) positions of the node
+	int y;
+
+	MapSearchNode() { x = y = 0; }
+	MapSearchNode(int px, int py) { x = px; y = py; }
+
+	float GoalDistanceEstimate(MapSearchNode &nodeGoal);
+	bool IsGoal(MapSearchNode &nodeGoal);
+	float GetCost(MapSearchNode &successor);
+	bool IsSameState(MapSearchNode &rhs);
+
+	void PrintNodeInfo();
+
+
+};
+
+bool MapSearchNode::IsSameState(MapSearchNode &rhs)
+{
+
+	// same state in a maze search is simply when (x,y) are the same
+	if ((x == rhs.x) &&
+		(y == rhs.y))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+
+
 
 AStar::AStar()
 {
